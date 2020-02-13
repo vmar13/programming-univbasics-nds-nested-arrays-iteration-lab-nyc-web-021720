a08ef83645895fbb8ad5fd5f1c681d45c45a29cf
Vanessa Martinez <vmar76@gmail.com>
2020-02-13 00:22:25 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -16,7 +16,6 @@ end
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
